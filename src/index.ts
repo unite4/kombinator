f313977ceadbd29ac5e67838d4359c5c9fab4d9f
Krzysztof Daniel <krzysztof.daniel@creativestyle.pl>
2023-04-16 18:44:48 +0200

    Typo fixes and exposing interface

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import copyComponents from './rollup-plugin-copy-components';
 import executeMods from './rollup-plugin-execute-mods';
 import copyFromParent from './rollup-plugin-nuxt-copy-from-parent';
 import { VueModifyTemplate } from './VueModifyTemplate';
+import { VueFileHandler } from './VueFileHandler';
+import { TemplateMod, getWithTemplate } from './helpers';
 
 export {
   copyComponents,
   executeMods,
   copyFromParent,
-  VueModifyTemplate
+  VueModifyTemplate,
+  VueFileHandler,
+  getWithTemplate,
+  TemplateMod
 }
--- src/rollup-plugin-copy-components.ts
+++ src/rollup-plugin-copy-components.ts
@@ -1,28 +1,29 @@
 import fsExtra from 'fs-extra';
 import path from 'path';
 import {globSync} from 'glob';
+import { Plugin, PluginContext } from 'rollup';
 
 interface CopyComponentsPluginOptions {
   sourceDirectories: string[];
   targetDirectory: string;
 }
 
-export default function copyComponentsPlugin(options: CopyComponentsPluginOptions) {
+export default function copyComponentsPlugin(options: CopyComponentsPluginOptions): Plugin {
   let initialized = false;
 
-  async function copyFiles() {
+  async function copyFiles(this: PluginContext) {
     // Copy each component file from the source directories to the target directory
     for (const sourceDirectory of options.sourceDirectories.reverse()) {
       const sourceFiles = globSync(`${sourceDirectory}/**/*.vue`);
-      if (!sourceFiles || sourceFiles.length === 0) {        
-        console.log(`Source directory ${sourceDirectory} is empty. Skipping.`);
+      if (!sourceFiles || sourceFiles.length === 0) {                
+        this.warn(`Source directory ${sourceDirectory} is empty. Skipping.`);
         continue;
       }
       
       for (const sourceFile of sourceFiles) {
         const relativePath = path.relative(sourceDirectory, sourceFile);
-        const targetFile = path.join(options.targetDirectory, relativePath);
-        console.log(`Copying ${sourceFile} tp ${targetFile}`)
+        const targetFile = path.join(options.targetDirectory, relativePath);        
+        // console.log(`Copying ${sourceFile} tp ${targetFile}`)
         await fsExtra.ensureDir(path.dirname(targetFile));
         await fsExtra.copyFile(sourceFile, targetFile);
       }
@@ -34,19 +35,10 @@ export default function copyComponentsPlugin(options: CopyComponentsPluginOption
     async buildStart() {
       // Only copy the files on the first build
       if (!initialized) {
-        await copyFiles();
+        await copyFiles.call(this);
         initialized = true;
-      }
-    },
-    async watchChange(filePath: string) {
-      console.log(filePath);
-      // Only copy the files if the changed file is a component in one of the source directories
-      for (const sourceDirectory of options.sourceDirectories) {
-        if (filePath.startsWith(sourceDirectory) && filePath.endsWith('.vue')) {
-          await copyFiles();
-          break;
-        }
-      }
-    },
+      }      
+    }
+    
   };
 }
