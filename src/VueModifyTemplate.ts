05e0ff070798990fc33fc748795ee6b2a0859fd4
Krzysztof Daniel <krzysztof.daniel@creativestyle.pl>
2023-04-18 22:52:42 +0200

    Moved all modifications to one class to avoid problems with chaining

--- src/VueModifyTemplate.ts
+++ src/VueModifyTemplate.ts
@@ -1,6 +1,6 @@
-import { VueModifyAttributes } from "./VueModifyAttributes";
+import { VueElementFinder } from "./VueElementFinder";
 
-export class VueModifyTemplate extends VueModifyAttributes {
+export class VueModifyTemplate extends VueElementFinder {
   public fromTemplate(template: string): VueModifyTemplate {
     super.fromTemplate(template);
     return this;
@@ -23,6 +23,72 @@ export class VueModifyTemplate extends VueModifyAttributes {
 
   public findFirst(): VueModifyTemplate {
     super.findFirst();
+    return this;
+  }
+
+  public removeAttribute(name: string): VueModifyTemplate {
+    const { element } = this.findElement(this.modifiedCode);
+    const modifiedElement = this.modifiedCode.slice(element.startIndex, element.endIndex).replace(new RegExp(` ${name}="([^"]*)?"`, 'g'), '').trim();
+    this.modifiedCode = this.modifiedCode.slice(0, element.startIndex) + modifiedElement + this.modifiedCode.slice(element.endIndex);
+    return this;
+  }
+  
+  public reduceAttribute(name: string, value: string): VueModifyTemplate {    
+    const { element } = this.findElement(this.modifiedCode);
+    let modifiedElement = this.modifiedCode.slice(element.startIndex, element.endIndex);
+    modifiedElement = modifiedElement.replace(new RegExp(` ${name}="([^"]*)"?`, 'g'), (match, p1) => {
+      const newValue = p1.replace(value, '').trim();
+      return newValue ? ` ${name}="${newValue}"` : '';
+    }).trim();
+    this.modifiedCode = this.modifiedCode.slice(0, element.startIndex) + modifiedElement + this.modifiedCode.slice(element.endIndex);
+  
+    return this;
+  }
+
+  public regexpAttribute(name: string, regexp: RegExp, replace: string): VueModifyTemplate {
+    const { element } = this.findElement(this.modifiedCode);
+    let modifiedElement = this.modifiedCode.slice(element.startIndex, element.endIndex);
+    modifiedElement = modifiedElement.replace(new RegExp(` ${name}="([^"]*)"?`), (match, p1) => {
+      if (!p1) {
+        throw new Error(`Attribute "${name}" does not exist.`);
+      }
+      return ` ${name}="${p1.replace(regexp, replace)}"`;
+    }).trim();
+    this.modifiedCode = this.modifiedCode.slice(0, element.startIndex) + modifiedElement + this.modifiedCode.slice(element.endIndex);
+    return this;
+  }
+
+  public extendAttribute(name: string, value: string, glue = ' '): VueModifyTemplate {
+    const { element } = this.findElement(this.modifiedCode);
+    let modifiedElement = this.modifiedCode.slice(element.startIndex, element.endIndex);
+    modifiedElement = modifiedElement.replace(new RegExp(` ${name}="([^"]*)"?`, 'g'), (match, p1) => {
+      const newValue = p1 ? `${p1.trim()}${glue}${value.trim()}` : value.trim();
+      return ` ${name}="${newValue}"`;
+    }).trim();
+    if (!modifiedElement.includes(name)) {
+      const newAttribute = ` ${name}="${value.trim()}"`;
+      const isSelfClosing = modifiedElement.endsWith('/>');
+      const indexOfClosingBracket = modifiedElement.lastIndexOf('>');
+      if (isSelfClosing) {
+        modifiedElement = `${modifiedElement.slice(0, -2)}${newAttribute}/>`;
+      } else {
+        modifiedElement = `${modifiedElement.slice(0, indexOfClosingBracket)}${newAttribute}${modifiedElement.slice(indexOfClosingBracket)}`;
+      }
+    }
+    this.modifiedCode = this.modifiedCode.slice(0, element.startIndex) + modifiedElement + this.modifiedCode.slice(element.endIndex);
+    return this;
+  }  
+  
+  public setAttribute(name: string, value: string): VueModifyTemplate {
+    const { element } = this.findElement(this.modifiedCode);
+    let modifiedElement = this.modifiedCode.slice(element.startIndex, element.endIndex);
+    const existingAttributeRegex = new RegExp(`\\s${name}="[^"]*"`);
+    if (existingAttributeRegex.test(modifiedElement)) {
+      modifiedElement = modifiedElement.replace(existingAttributeRegex, ` ${name}="${value}"`);
+    } else {
+      modifiedElement = modifiedElement.replace(/(\/?>)$/, ` ${name}="${value}"$1`);
+    }
+    this.modifiedCode = this.modifiedCode.slice(0, element.startIndex) + modifiedElement + this.modifiedCode.slice(element.endIndex);
     return this;
   }
 
